41af1aa93cd0f6732cb65c3467ee7d61ac612224
Ali ŞİMŞEK <ali.simsek@technoface.com.tr>
2023-09-28 17:52:19 +0300

    test

--- src/projects/tipMe/webAPI.Application/Features/Tips/Commands/Update/UpdateTipCommand.cs
+++ src/projects/tipMe/webAPI.Application/Features/Tips/Commands/Update/UpdateTipCommand.cs
@@ -13,15 +13,15 @@ namespace Application.Features.Tips.Commands.Update;
 public class UpdateTipCommand : IRequest<CustomResponseDto<UpdatedTipResponse>>
 {
     public Guid Id { get; set; }
-    public DateTime RequestDate { get; set; }
-    public string QrCode { get; set; }
+    //public DateTime RequestDate { get; set; }
+    // public string QrCode { get; set; }
     public decimal? TipAmount { get; set; }
-    public bool? IsTipped { get; set; }
-    public DateTime? PaymentDate { get; set; }
+    ///public bool? IsTipped { get; set; }
+    //public DateTime? PaymentDate { get; set; }
     public string? PaymentReference { get; set; }
-    public bool? IsCommented { get; set; }
+    //public bool? IsCommented { get; set; }
     public string? Comment { get; set; }
-    public DateTime? CommentDate { get; set; }
+    //public DateTime? CommentDate { get; set; }
     public int? Point { get; set; }
     public List<Guid> Options { get; set; }
 
@@ -50,10 +50,18 @@ public class UpdateTipCommand : IRequest<CustomResponseDto<UpdatedTipResponse>>
             Tip? tip = await _tipRepository.GetAsync(predicate: t => t.Id == request.Id, cancellationToken: cancellationToken);
             await _tipBusinessRules.TipShouldExistWhenSelected(tip);
             tip = _mapper.Map(request, tip);
+            tip.IsTipped = true;
+            tip.IsCommented = tip.Comment.Length > 0;
+            tip.CommentDate = tip.Comment.Length > 0 ? DateTime.Now : null;
+            tip.PaymentDate = DateTime.Now;
 
             await _tipRepository.UpdateAsync(tip!);
 
             Invoice? invoice = await _invoiceRepository.GetAsync(predicate: x => x.QrCode == tip.QrCode, cancellationToken: cancellationToken);
+            invoice.IsTipped = true;
+            invoice.TipDate = DateTime.Now;
+            await _invoiceRepository.UpdateAsync(invoice);
+
             foreach (var item in request.Options)
             {
                 await _invoiceOptionRepository.AddAsync(new() { InvoiceId = invoice.Id, OptionId = item });
