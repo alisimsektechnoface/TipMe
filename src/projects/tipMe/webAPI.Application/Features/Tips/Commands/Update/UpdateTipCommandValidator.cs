0294c0febec8d4a6bcac200dc51523f0160b7263
Ali ŞİMŞEK <ali.simsek@technoface.com.tr>
2023-09-28 17:56:46 +0300

    bug fix

--- src/projects/tipMe/webAPI.Application/Features/Tips/Commands/Update/UpdateTipCommandValidator.cs
+++ src/projects/tipMe/webAPI.Application/Features/Tips/Commands/Update/UpdateTipCommandValidator.cs
@@ -7,14 +7,14 @@ public class UpdateTipCommandValidator : AbstractValidator<UpdateTipCommand>
     public UpdateTipCommandValidator()
     {
         RuleFor(c => c.Id).NotEmpty();
-        RuleFor(c => c.RequestDate).NotEmpty();
-        RuleFor(c => c.QrCode).NotEmpty();
-        RuleFor(c => c.IsTipped).NotEmpty();
-        RuleFor(c => c.PaymentDate).NotEmpty();
+        //RuleFor(c => c.RequestDate).NotEmpty();
+        //RuleFor(c => c.QrCode).NotEmpty();
+        //RuleFor(c => c.IsTipped).NotEmpty();
+        //RuleFor(c => c.PaymentDate).NotEmpty();
         RuleFor(c => c.PaymentReference).NotEmpty();
-        RuleFor(c => c.IsCommented).NotEmpty();
+        //RuleFor(c => c.IsCommented).NotEmpty();
         RuleFor(c => c.Comment).NotEmpty();
-        RuleFor(c => c.CommentDate).NotEmpty();
+        //RuleFor(c => c.CommentDate).NotEmpty();
         RuleFor(c => c.Point).NotEmpty();
     }
 }
