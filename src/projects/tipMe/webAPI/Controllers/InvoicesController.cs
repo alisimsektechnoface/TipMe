6741e6272d2b677690d98f3674395667c985b74f
Ali ŞİMŞEK <ali.simsek@technoface.com.tr>
2023-09-28 17:53:13 +0300

    change

--- src/projects/tipMe/webAPI/Controllers/InvoicesController.cs
+++ src/projects/tipMe/webAPI/Controllers/InvoicesController.cs
@@ -1,10 +1,5 @@
 using Application.Features.Invoices.Commands.Create;
-using Application.Features.Invoices.Commands.Update;
-using Application.Features.Invoices.Queries.GetById;
 using Application.Features.Invoices.Queries.GetByQrCode;
-using Application.Features.Invoices.Queries.GetList;
-using Core.Application.Requests;
-using Core.Application.Responses;
 using Core.Application.ResponseTypes.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using webAPI.Controllers.Base;
@@ -23,13 +18,13 @@ public class InvoicesController : BaseController
         return Created(uri: "", response);
     }
 
-    [HttpPut]
-    public async Task<IActionResult> Update([FromBody] UpdateInvoiceCommand updateInvoiceCommand)
-    {
-        CustomResponseDto<UpdatedInvoiceResponse> response = await Mediator.Send(updateInvoiceCommand);
+    //[HttpPut]
+    //public async Task<IActionResult> Update([FromBody] UpdateInvoiceCommand updateInvoiceCommand)
+    //{
+    //    CustomResponseDto<UpdatedInvoiceResponse> response = await Mediator.Send(updateInvoiceCommand);
 
-        return Ok(response);
-    }
+    //    return Ok(response);
+    //}
 
     //[HttpDelete("{id}")]
     //public async Task<IActionResult> Delete([FromRoute] Guid id)
@@ -39,20 +34,20 @@ public class InvoicesController : BaseController
     //    return Ok(response);
     //}
 
-    [HttpGet("{id}")]
-    public async Task<IActionResult> GetById([FromRoute] Guid id)
-    {
-        CustomResponseDto<GetByIdInvoiceResponse> response = await Mediator.Send(new GetByIdInvoiceQuery { Id = id });
-        return Ok(response);
-    }
+    //[HttpGet("{id}")]
+    //public async Task<IActionResult> GetById([FromRoute] Guid id)
+    //{
+    //    CustomResponseDto<GetByIdInvoiceResponse> response = await Mediator.Send(new GetByIdInvoiceQuery { Id = id });
+    //    return Ok(response);
+    //}
 
-    [HttpGet]
-    public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
-    {
-        GetListInvoiceQuery getListInvoiceQuery = new() { PageRequest = pageRequest };
-        CustomResponseDto<GetListResponse<GetListInvoiceListItemDto>> response = await Mediator.Send(getListInvoiceQuery);
-        return Ok(response);
-    }
+    //[HttpGet]
+    //public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+    //{
+    //    GetListInvoiceQuery getListInvoiceQuery = new() { PageRequest = pageRequest };
+    //    CustomResponseDto<GetListResponse<GetListInvoiceListItemDto>> response = await Mediator.Send(getListInvoiceQuery);
+    //    return Ok(response);
+    //}
 
 
     [HttpGet("GetByQrCode/{QrCode}")]
