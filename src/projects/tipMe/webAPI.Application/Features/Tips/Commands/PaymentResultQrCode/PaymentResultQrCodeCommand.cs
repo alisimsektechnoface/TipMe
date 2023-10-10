0efa72382fdc20985c87ce7b5b4eac391dfc4e21
Ali ŞİMŞEK <ali.simsek@technoface.com.tr>
2023-10-10 17:02:42 +0300

    test

--- src/projects/tipMe/webAPI.Application/Features/Tips/Commands/PaymentResultQrCode/PaymentResultQrCodeCommand.cs
+++ src/projects/tipMe/webAPI.Application/Features/Tips/Commands/PaymentResultQrCode/PaymentResultQrCodeCommand.cs
@@ -36,7 +36,10 @@ public class PaymentResultQrCodeCommand : IRequest<CustomResponseDto<CheckoutFor
         {
             CheckoutForm checkoutForm = await _tipsService.PaymentResultQrCode(request.QrCode);
 
-            if (checkoutForm?.PaymentStatus.ToLower() == Status.SUCCESS.ToString() && request.QrCode != "A3E96248-C0DE-4CE7-889E-9246E868CB90-0574A69D-3C65-4409-8166-3F7CE90153EA")
+            if (request.QrCode == "A3E96248-C0DE-4CE7-889E-9246E868CB90-0574A69D-3C65-4409-8166-3F7CE90153EA")
+                return CustomResponseDto<CheckoutForm>.Success((int)HttpStatusCode.OK, checkoutForm, checkoutForm.PaymentStatus.ToLower() == Status.SUCCESS.ToString());
+
+            if (checkoutForm?.PaymentStatus.ToLower() == Status.SUCCESS.ToString())
             {
                 Invoice? invoice = await _invoiceRepository.GetAsync(x => x.QrCode == request.QrCode, enableTracking: false, cancellationToken: cancellationToken);
                 invoice.IsTipped = true;
