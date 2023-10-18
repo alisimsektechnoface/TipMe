9e990295fcd675d2b707d5d155eeeef82fc33de6
Ali ŞİMŞEK <ali.simsek@technoface.com.tr>
2023-10-18 13:03:41 +0300

    test

--- src/projects/tipMe/webAPI.Application/Features/Tips/Commands/PaymentResult/PaymentResultCommand.cs
+++ src/projects/tipMe/webAPI.Application/Features/Tips/Commands/PaymentResult/PaymentResultCommand.cs
@@ -1,10 +1,8 @@
-using Application.Features.Tips.Constants;
 using Application.Features.Tips.Rules;
 using Application.Services.Repositories;
 using Application.Services.Tips;
 using AutoMapper;
 using Core.Application.ResponseTypes.Concrete;
-using Core.CrossCuttingConcerns.Exceptions.Types;
 using Core.Domain.Entities;
 using Iyzipay.Model;
 using MediatR;
@@ -49,8 +47,8 @@ public class PaymentResultCommand : IRequest<CustomResponseDto<CheckoutForm>>
                 await _invoiceRepository.UpdateAsync(invoice);
 
             }
-            else
-                throw new BusinessException(TipsBusinessMessages.TipNotExists);
+            //else
+            //  throw new BusinessException(TipsBusinessMessages.TipNotExists);
 
             return CustomResponseDto<CheckoutForm>.Success((int)HttpStatusCode.OK, checkoutForm, checkoutForm.PaymentStatus == Status.SUCCESS.ToString());
         }
